d569ca8a24e1c77e0612697e38245bc3e429028b
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-02-02 21:38:52 +0000

    articles append to html

--- script.js
+++ script.js
@@ -6,13 +6,26 @@ let endY = $("#end-year");
 let dateCorrector = "0101";
 
 
-function ajaxHandler (a, c, d) {
+function ajaxHandler (a, b, c, d) {
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + a + "&api-key=qA6nugDJGMyJMS1kZDGL9zq4AW0tpING&begin_date=" + c + "&end_date=" + d;
   $.ajax({
     url: queryURL,
     method: "GET"    
   }).then(function(response) {
     console.log(response);
+    console.log(b);
+    
+    for(let i = 0; i < b; i++){
+      let newsDiv = $("<div>");
+      let title = $("<p class='lead' style='font-weight:bold;'>").text(response.response.docs[i].abstract);
+      let date = $("<p>").text(response.response.docs[i].pub_date);
+      let snippet = $("<p>").text(response.response.docs[i].snippet);
+      newsDiv.append(title, date, snippet);
+      $("#results").append(newsDiv);
+    
+    }
+    
+
   })
 }
 
@@ -23,12 +36,12 @@ function ajaxHandler (a, c, d) {
 
 $("#search").on("click", function(){
     let querySearch = searchTerm.val().trim();
-    let queryRecordNo = searchTerm.val().trim();
+    let queryRecordNo = recordNo.val().trim();
     let queryStart = startY.val().trim();
     queryStart += dateCorrector
     // console.log(queryStart);
     let queryEnd = endY.val().trim()
     queryEnd += dateCorrector
     // console.log(queryEnd);
-    ajaxHandler(querySearch, queryStart, queryEnd)
+    ajaxHandler(querySearch, queryRecordNo, queryStart, queryEnd)
 })
